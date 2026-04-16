cb37e334b9fd5d3f031ed6a1734f24f6fc691779
Adiana Afida <adianafida2215@gmail.com>
2026-04-16 15:48:56 +0700

    fix: perbaikan footer dan migration logos

--- database/migrations/2026_03_27_074855_add_name_to_logos_table.php
+++ database/migrations/2026_03_27_074855_add_name_to_logos_table.php
@@ -1,16 +1,25 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
-    {
-        //
-    }
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('logos', function (Blueprint $table) {
+        $table->string('name')->nullable()->after('type'); // nama client, nullable
+    });
+}
 
-    public function down(): void
-    {
-        //
-    }
+public function down()
+{
+    Schema::table('logos', function (Blueprint $table) {
+        $table->dropColumn('name');
+    });
+}
 };
